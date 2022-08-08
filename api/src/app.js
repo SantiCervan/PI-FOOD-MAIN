e9c77b13896318d39ed48845aeea4186e0c5e626
unknown <santi.cervan.12@gmail.com>
2022-08-08 19:23:57 -0300

    cambios para deployar 6

--- api/src/app.js
+++ api/src/app.js
@@ -23,6 +23,7 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+server.use(cors());
 
 server.use('/', routes);
 
